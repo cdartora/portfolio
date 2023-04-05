35faf7e7887f062cdde304ff8e47fc6830e30563
Carlos Dartora <carlosdalsoler@gmail.com>
2023-04-05 17:28:07 -0300

    setup phosphor react

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
 import "./App.css";
+import { BookmarkSimple } from "phosphor-react";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
+    <div className="flex flex-col items-center">
       <h1 className="leading-relaxed text-red-300">Hello, world</h1>
-      <button onClick={() => setCount(count + 1)}>{count}</button>
+      <div className="flex">
+        <button onClick={() => setCount(count + 1)}>{count}</button>
+        <BookmarkSimple size={32} weight="fill" />
+      </div>
     </div>
   );
 }
